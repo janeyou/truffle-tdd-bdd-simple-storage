be1cf5648e97137197f0b6b2a930612840c759cf
Jane You <jane2745@gmail.com>
2022-01-09 11:05:52 -0800

    set value

--- test/simple_storage.js
+++ test/simple_storage.js
@@ -5,7 +5,7 @@ const SimpleStorage = artifacts.require("SimpleStorage");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("SimpleStorage", function (/* accounts */) {
+contract("SimpleStorage", function (accounts) {
   it("should assert true", async function () {
     await SimpleStorage.deployed();
     return assert.isTrue(true);
@@ -15,5 +15,15 @@ contract("SimpleStorage", function (/* accounts */) {
     const ssInstance = await SimpleStorage.deployed();
     const storedData = await ssInstance.getStoredData.call();
     assert.equal(storedData, 0, `Initial state should be zero`);
+  });
+
+  describe("Functionality", () => {
+    const dataExpected = 42;
+    it(`should store the value ${dataExpected}`, async () => {
+      const ssInstance = await SimpleStorage.deployed();
+      await ssInstance.setStoredData(dataExpected, { from: accounts[0] });
+      const storedData = await ssInstance.getStoredData.call();
+      assert.equal(storedData, dataExpected, `${dataExpected} was not stored!`);
+    });
   });
 });
