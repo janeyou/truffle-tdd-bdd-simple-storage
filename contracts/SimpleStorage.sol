6b0324b9dfefceca836813cb20e0b307895e20d6
Jane You <jane2745@gmail.com>
2022-01-09 11:17:20 -0800

    add account and contructor initial value 0

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,12 +3,18 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract SimpleStorage {
     uint256 public storedData;
+    address owner = msg.sender;
+
+    constructor(uint256 _num) public {
+        storedData = _num;
+    }
 
     function getStoredData() public view returns (uint256) {
         return storedData;
     }
 
     function setStoredData(uint256 x) public {
+        require(msg.sender == owner, "Not the owner!");
         storedData = x;
     }
 }
